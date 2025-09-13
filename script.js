6dbe626b4639b86b0f03a33709496e96bcfd2fe6
diyorbek <diyka0007@gmail.com>
2025-09-13 17:53:50 +0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-let num = 1
+let num = 120
 
-if (num % 2){
-    console.log("toq");
-}else{
-    console.log("juft");
+switch (num) {
+    case 1:
+    console.log("yanvar");
+    break;
+
+    case 2:
+    console.log("fevral");
+    break;
+    
+    case 3:
+    console.log("mart");
+    break;
+
+    case 4:
+    console.log("aprel");
+    break;
+
+    case 5:
+    console.log("may");
+    break;
+
+    case 6:
+    console.log("iyun");
+    break;
+
+    case 7:
+    console.log("iyul");
+    break;
+    
+    case 8:
+    console.log("avgust");
+    break;
+
+    case 9:
+    console.log("sentabr");
+    break;
+
+    case 10:
+    console.log("oktyabr");
+    break;
+
+    case 11:
+    console.log("noyabr");
+    break;
+
+    case 12:
+    console.log("dekabr");
+    break;
+    
+    default:
+    console.log("e quli qiyshiq nechchini kiritopsan");
+    break;
 }
